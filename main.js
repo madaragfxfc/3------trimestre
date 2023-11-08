fe284cfb8040f9dd7bb154f1af53ecc8d9f42d16
madaragfxfc <125053616+madaragfxfc@users.noreply.github.com>
2023-11-08 11:28:40 -0300

    Add files via upload

--- main.js
+++ main.js
@@ -2,20 +2,27 @@ function tocaSom (idElementAudio) {
     document.querySelector(idElementAudio).play();
 }
 
-const listaDeTeclas = document.querySelectorAll('.tecla'); 
+const ListaDeTeclas = document.querySelectorAll('.tecla')
 
 let contador = 0;
 
 //enquanto
-while (contador < listaDeTeclas.length) {
+while (contador < ListaDeTeclas.length) {
 
-    listaDeTeclas[contador].onclick = function () { 
-        tocaSom('#som_tecla_pom');
-    }
+    const tecla = ListaDeTeclas [contador] 
+    const instrumento = tecla.classList[1];
+    //template string
+    const idAudio = `#som_${instrumento}`;
+    
+    // console.log(idAudio);
+ 
+    tecla.onclick = function () { 
+        tocaSom(idAudio);
+     }
 
     contador = contador + 1;
 
-    console.log(contador);
+    // console.log(contador);
 
 }
 
